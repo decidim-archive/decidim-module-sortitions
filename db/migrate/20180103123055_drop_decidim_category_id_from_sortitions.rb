c6c74473b312efc3c5a7e9b47c20aa114f810019
Juan Salvador Perez Garcia <jsperezg@gmail.com>
2018-01-08 17:26:39 +0100

    Fixed migrations after Module namespace removal

--- db/migrate/20180103123055_drop_decidim_category_id_from_sortitions.rb
+++ db/migrate/20180103123055_drop_decidim_category_id_from_sortitions.rb
@@ -2,13 +2,6 @@
 
 class DropDecidimCategoryIdFromSortitions < ActiveRecord::Migration[5.1]
   def up
-    Decidim::Module::Sortitions::Sortition.where.not(decidim_category_id: nil).each do |sortition|
-      Decidim::Categorization.create!(
-        decidim_category_id: sortition.decidim_category_id,
-        categorizable: sortition
-      )
-    end
-
     remove_column :decidim_module_sortitions_sortitions, :decidim_category_id
   end
 
